b398c981f89dbe9a0fb203815a64a924c8bf7754
Joe Dralliam <jdralliam@gmail.com>
2014-10-21 02:13:59 +0200

    Starting HaskellF !!!!

--- typer.ml
+++ typer.ml
@@ -1,64 +1,67 @@
-
 open Ast
 
-module TVar : sig
-  type t = private int
-  val fresh : unit -> t
-end = struct
-  type t = int
-  let last = ref 0
-  let fresh () =
-    let () = last := 1 + !last in
-    !last
+module Var =
+struct
+  type t =  int
+  let fresh =
+    let counter = ref 0 in
+    fun () -> (incr counter ; !counter)
 end
 
-type tvar = TVar.t
-type ty = (* no tuples :( *)
-  | TVar of tvar
-  | TInt
-  | TBool
-  | TChar
-  | TList of ty
-  | TFun of ty * ty
-  (* TODO : see do and return *)
-type ty_sch = tvar list * ty
+type ty =
+  | TConst of string * ty list
+  | TVar of Var.t 
+  | TArrow of ty * ty
 
-module VarSet = Set.Make (struct type t = tvar
-                                 let compare = Pervasives.compare end)
-(* construct types *)
-let ty t = ([], t)
-let all x (l, t) = (x::l, t)
 
-let rec fun_of l b = match l with
-  | [] -> Utils.exhaust_pattern ()
-  | [x] -> TFun (x, b)
-  | h :: t -> TFun (h, fun_of t b)
+type bound =
+  | BRigid
+  | BFlexible
 
-let rec subst t1 x1 t = match t with
-  | TVar v -> if v = x1 then t1 else t
-  | TInt | TBool | TChar -> t
-  | TList t2 -> TList (subst t1 x1 t2)
-  | TFun (t2, t3) -> TFun (subst t1 x1 t2, subst t1 x1 t3)
+type schema =
+  | SBot
+  | STy of ty
+  | SForall of constr * schema
+ and constr = Var.t * (bound * schema)
 
-(* typed ast *)
-type 'a typed_ast = 'a * ty_sch ast
 
-let set_type (ast : 'a ast) sty : 'a typed_ast = (fst ast, (sty, snd ast))
-let get_type (ast : 'a typed_ast) : ty_sch = fst (snd ast)
+type env = (var * schema) list
 
-let infer_equs env ast =
-  let stack = Stack.create () in
-  let rec aux env ast = match snd ast with
-    | Int _ -> set_type ast (ty TInt)
-    | Bool _ -> set_type ast (ty TBool)
-    | Char _ -> set_type ast (ty TChar)
-    | Op _ -> failwith "TODO: Op"
-    | Id _ -> failwith "TODO: Id"
-    | Lambda (lx, body) ->
-       let fresh_vars = List.map (fun _ -> TVar (TVar.fresh ())) lx in
-       let ty_body =
-         get_type (aux ((List.combine (List.map ty fresh_vars) lx) @ env)
-                       body) in
-       set_type ast (ty (fun_of fresh_vars ty_body))
-    | _ -> failwith "TODO: rest"
-  in ()
+let split q2 q =
+  List.split (fun (x,_) -> List.filter (fun (y,_) -> x = y) q) q2
+
+
+let forall_map q4 sch =
+  List.fold_left (fun sch constr ->
+		  SForall (constr,sch)
+		 ) sch q4
+			  
+let rec infer q env = function
+  | Const _ -> (q, STy (TConst ("string",[])))
+  | Var x -> (q, List.assoc x env)
+  | Abstr (x,a) ->
+     let alpha = Var.fresh () in
+     let q1,sigma = (alpha, (BFlexible, SBot)) :: q in
+     let q2 = infer q1 ((x,STy (TVar alpha)) :: gamma) in
+     let beta = Var.fresh () in
+     let (q3,q4) = split q2 q in
+     q3, forall_map q4 (SForall ((beta,BFlexible,sigma),
+				 STy (TArrow (TVar alpha,TVar beta))))
+  | App (a,b) ->
+     let q1,sigma_a = infer q1 env a in
+     let q2,sigma_b = infer q2 env b in
+     let alpha_a = Var.fresh () in
+     let alpha_b = Var.fresh () in
+     let beta = Var.fresh () in
+     let q3 = unify ((beta, BFlexible, SBot)
+		     :: (alpha_b, BFlexible, sigma_b)
+		     :: (alpha_a, BFlexible, sigma_a)
+		     :: q2)
+		    alpha_a alpha_b 
+     in
+     let q4,q5 = split q3 q in
+     q4, forall_map q5 (STy (TVar beta))
+  | Let (x,a1,a2) ->
+     let q1,sigma1 = infer q env a1 in
+     infer q1 ((x,sigma1) :: gamma) a2
+		    
