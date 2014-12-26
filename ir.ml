2f6c78af36ef8d3a8ca0a538d1a1d72bcc18b134
Theo Laurent <theo.laurent@gmx.fr>
2014-12-26 13:27:15 +0100

    Second draft.

--- ir.ml
+++ ir.ml
@@ -4,63 +4,62 @@ open Ast
 
 
 (* Inspired from UPMC'LI223 ; is it a good intermediate rep ?? *)
-type value = unit (* TODO *)
+(* closures => not chained env ; closure = code + env *)
+(* TODO : handle recursive definitions *)
 type label = L of int
-type addr = A of int
-type prim =
-  | Add
-  | Sub
-  | Mult
-  | Div
+type env = value list
+and value =
+  | Imm of const   (* TODO : what about lists ?? *)
+  | Clos of label (* TODO : at next compilation step, handle environement *)
+                  (* will use a register to handle environement *)
+  | Froz of unit (* TODO *)
 type ir =
-  | NoOp (* useless ? *)
   | Label of label
   | Push of value
-  | Pop
-  | Jump of label
-  | JumpFalse of label
+  | Branch of label
+  | BranchFalse of label
   | CallFun (* no need of number arg, always one *)
-  | CallPrim of prim
-  | Return
+  | ReturnCall
   (* lexical bindings *)
-  | Fetch of addr
-  | Store of addr
-  (* Global definitions ; useless ? *)
-  | GAlloc
-  | GFetch of addr
-  | Gstore of addr
+  | Fetch of int
 
-(* TODO : don't use lists ! *)
+
+(* TODO : don't use lists for sequences ! *)
+
 
 let next_label =
   let n = ref (-1) in
   (fun () -> incr n ; L !n)
 
-let compile_const c = match c with
-  | CUnit -> failwith "TODO"
-  | CBool b -> failwith "TODO"
-  | CInt i -> failwith "TODO"
-  | CChar c -> failwith "TODO"
-  | CPrim p -> failwith "TODO"
+let index l x =
+  let rec loop l i = match l with
+  | [] -> raise Not_found
+  | h :: t -> if h = x then i else loop t (i + 1)
+  in loop l 0
 
-let compile_var v = failwith "TODO"
-
-let rec compile_expr ast = match ast.data with
-  | Const c -> compile_const c
-  | Var v -> compile_var v
-  | Abstr _ -> failwith "TODO"
-  | App _ -> failwith "TODO"
+let rec expr_to_ir env ast = match ast.data with
+  | Const c -> [ Push (Imm c) ]
+  | Var v -> [ Fetch (index env v) ]
+  (* /\ TODO : what about primitives ? *)
+  | Abstr ({ data = v}, body) ->
+     let lfunc = next_label () in
+     let lcont = next_label () in
+     [ Branch lcont ; Label lfunc ]
+     @ (expr_to_ir (v :: env) body)
+     @ [ ReturnCall ; Label lcont ; Push (Clos lfunc) ]
+  | App (f, e) ->
+     (expr_to_ir env e) @ (expr_to_ir env f) @ [ CallFun ] (* TODO : et call prim ?? *)
   | Let _ -> failwith "TODO"
-  | Spec s -> compile_spec s
-and compile_spec s = match s with
+  | Spec s -> spec_to_ir env s
+and spec_to_ir env s = match s with
   | If (cond, ontrue, onfalse) ->
      let lfalse = next_label () in
      let lcont = next_label () in
-     (compile_expr cond)
-     @ [ JumpFalse lfalse ]
-     @ (compile_expr ontrue)
-     @ [ Jump lcont ; Label lfalse ]
-     @ (compile_expr onfalse)
+     (expr_to_ir env cond)
+     @ [ BranchFalse lfalse ]
+     @ (expr_to_ir env ontrue)
+     @ [ Branch lcont ; Label lfalse ]
+     @ (expr_to_ir env onfalse)
      @ [ Label lcont ]
   | Case _ -> failwith "TODO"
   (* why not just expand case into if ? *)
